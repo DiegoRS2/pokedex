33bb8db94f39c46a5836bf5ae04c8a5013cb1fd1
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2022-04-10 17:04:02 -0300

    Update pokemon.model.ts

--- pokedex/src/app/pages/service/pokemon.model.ts
+++ pokedex/src/app/pages/service/pokemon.model.ts
@@ -30,7 +30,8 @@ export class Pokemons {
       public front_default: string,
       public front_female: string,
       public front_shiny: string,
-      public front_shiny_female: string
+      public front_shiny_female: string,
+      
     ) {}
   }
   
