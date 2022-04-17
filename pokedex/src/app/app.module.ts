19ba3adb179ee9059c33c3034137f26d2215ea3c
DiegoRS2 <72531642+DiegoRS2@users.noreply.github.com>
2022-04-17 14:52:21 -0300

    Update app.module.ts

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './template/footer/footer.component';
 import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxPaginationModule }  from  'ngx-pagination' ;
+import { NgxPaginationModule } from 'ngx-pagination';
 import { FormsModule } from '@angular/forms';
 import { InformationsComponent } from './pages/informations/informations.component';
 import { RouterModule } from '@angular/router';
